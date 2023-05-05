5c34162167e5be9d5c018605210cad5a55a62406
HCH <2762312941@qq.com>
2023-05-05 23:03:01 +0800

    添加了一些测试用例

--- test_bench/offer_post.sh
+++ test_bench/offer_post.sh
@@ -26,3 +26,24 @@ curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
 --form 'time_limit="2023-05-05 17:29:02"'
 
 echo '\n'
+
+# 检验与User的关联是否正确
+curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
+--form 'reward_amount="300.0"' \
+--form 'customer_id="1000000"' \
+--form 'category_name="跑腿"' \
+--form 'pickup_location_id="1"' \
+--form 'delivery_location_id="1"' \
+--form 'time_limit="2023-05-05 17:29:02"'
+
+echo '\n'
+
+# 没有category
+curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
+--form 'reward_amount="300.0"' \
+--form 'customer_id="2"' \
+--form 'pickup_location_id="1"' \
+--form 'delivery_location_id="1"' \
+--form 'time_limit="2023-05-05 17:29:02"'
+
+echo '\n'
