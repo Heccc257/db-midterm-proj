ae53d7311fd71977bca0b9a8d82e16dac1748bf4
HCH <2762312941@qq.com>
2023-05-05 22:33:04 +0800

    更新了test_bench

---
+++ test_bench/offer_post.sh
@@ -0,0 +1,28 @@
+curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
+--form 'reward_amount="200.0"' \
+--form 'customer_id="1"' \
+--form 'category_name="外卖"' \
+--form 'pickup_location_id="1"' \
+--form 'delivery_location_id="1"' \
+--form 'time_limit="2023-05-05 17:29:02.295078175"'
+
+echo '\n'
+
+curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
+--form 'customer_id="1"' \
+--form 'category_name="跑腿"' \
+--form 'pickup_location_id="1"' \
+--form 'delivery_location_id="1"' \
+--form 'time_limit="2023-05-05 17:29:02.295078175"'
+
+echo '\n'
+
+curl --location --request POST 'http://127.0.0.1:9999/offer/post' \
+--form 'reward_amount="300.0"' \
+--form 'customer_id="2"' \
+--form 'category_name="跑腿"' \
+--form 'pickup_location_id="1"' \
+--form 'delivery_location_id="1"' \
+--form 'time_limit="2023-05-05 17:29:02"'
+
+echo '\n'
---
+++ test_bench/user_info_test.sh
@@ -0,0 +1,9 @@
+	
+curl --location --request GET 'http://127.0.0.1:9999/user_info/1'
+echo '\n'
+
+curl --location --request GET 'http://127.0.0.1:9999/user_info/-1'
+echo '\n'
+
+curl --location --request GET 'http://127.0.0.1:9999/user_info/3'
+echo '\n'
