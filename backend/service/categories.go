f97c6c4ea28dbc5f6ea80c74e185b143ad7a0709
HCH <2762312941@qq.com>
2023-05-06 00:20:46 +0800

    StartUp时会添加初始的category列表；完成了/categories请求，可以获取category列表

---
+++ backend/service/categories.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"fmt"
+	"net/http"
+	"server/service/dal/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Categories(c *gin.Context) {
+	var categories []model.OfferCategory
+	if err := db.Find(&categories).Error; err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	fmt.Println(categories)
+	c.JSON(http.StatusOK, categories)
+}
