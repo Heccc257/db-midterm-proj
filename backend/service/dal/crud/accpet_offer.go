5a73e3aad5e847c660c1d82a55d99cc895fac8bb
HCH <2762312941@qq.com>
2023-05-08 20:19:06 +0800

    完成订单，使用了事务

---
+++ backend/service/dal/crud/accpet_offer.go
@@ -0,0 +1,26 @@
+package crud
+
+import (
+	"fmt"
+	"server/service/dal/model"
+
+	"gorm.io/gorm"
+)
+
+func FindAcceptOffer(db *gorm.DB, offer_id, uid uint, ao *model.AcceptOffer) error {
+	db.Model(&model.AcceptOffer{}).Where("offer_id = ?", offer_id).Where("user_id = ?", uid).Find(ao)
+	if ao.OfferId != offer_id || ao.UserId != uid {
+		return fmt.Errorf("未找到对应表项")
+	}
+	return nil
+}
+
+func AcceptOfferModifyState(tx *gorm.DB, offer_id, uid uint, state string) error {
+	var accept_offer model.AcceptOffer
+	if tx.Model(&model.AcceptOffer{}).Where("offer_id = ?", offer_id).Where("uid = ?", uid).Find(&accept_offer); accept_offer.OfferId == 0 {
+		return fmt.Errorf("未找到对应accept_offer")
+	}
+	accept_offer.TaskState = state
+	tx.Save(&accept_offer)
+	return nil
+}
