054b9d3198400349020c776000e2bcdc7b2c6ffd
HCH <2762312941@qq.com>
2023-05-08 12:58:39 +0800

    .

--- pku_helper.sql
+++ pku_helper.sql
@@ -4,7 +4,7 @@
 create table user
 (
    user_id              int unsigned AUTO_INCREMENT not null,
-   nickname             varchar(20) not null,
+   nick_name             varchar(20) not null,
    full_name            varchar(20) not null,
    phone_number         varchar(20) not null,
    password_hash        char(64) not null,
