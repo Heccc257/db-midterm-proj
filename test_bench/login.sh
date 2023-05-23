3422feb6ae37ecb4b999ea30e294a2936692a329
hch <>
2023-05-23 16:25:14 +0800

    .

--- test_bench/login.sh
+++ test_bench/login.sh
@@ -1,30 +1,30 @@
-curl --location --request GET 'http://127.0.0.1:9999/login' \
+curl --location --request POST 'http://127.0.0.1:9999/login' \
 --form 'phone_number="123456"' \
 --form 'password="pass_word"'
 
 echo '\n'
 
 # wrong phone
-curl --location --request GET 'http://127.0.0.1:9999/login' \
+curl --location --request POST 'http://127.0.0.1:9999/login' \
 --form 'phone_number="123456xxxx"' \
 --form 'password="pass_word"'
 
 echo '\n'
 
 # wrong pass
-curl --location --request GET 'http://127.0.0.1:9999/login' \
+curl --location --request POST 'http://127.0.0.1:9999/login' \
 --form 'phone_number="123456"' \
 --form 'password="pass_wordxxxx"'
 
 echo '\n'
 
-curl --location --request GET 'http://127.0.0.1:9999/login' \
+curl --location --request POST 'http://127.0.0.1:9999/login' \
 --form 'phone_number="54321"' \
 --form 'password="pass_word_bob"'
 
 echo '\n'
 
-curl --location --request GET 'http://127.0.0.1:9999/login' \
+curl --location --request POST 'http://127.0.0.1:9999/login' \
 --form 'phone_number="999999"' \
 --form 'password="pass_word_carol"'
 
