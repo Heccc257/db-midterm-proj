ac8f481ff0b41fec38d80ba5c301b41b567c3692
hch <>
2023-05-25 14:17:32 +0800

    accepet_offer返回的是订单列表

--- backend/service/accept_offer_list.go
+++ backend/service/accept_offer_list.go
@@ -1,7 +1,8 @@
 package service
 
 import (
-	"server/service/dal/crud"
+	"fmt"
+	"server/service/dal/model"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -12,9 +13,26 @@ func AcceptOfferList(c *gin.Context) {
 		responseBadRequest(c, "输入正确的uid")
 		// c.String(http.StatusBadRequest, "输入正确的uid")
 	} else {
-		accpet_offer_list := *crud.AcceptOfferListUser(uint(uid), db)
-		responseOK(c, accpet_offer_list)
+		var offer_list []model.Offer
+
+		// offer_id 相等
+		db.Table("offer").
+			Joins("NATURAL JOIN accept_offer").
+			Where("user_id = ?", uid).
+			Select("*").Scan(&offer_list)
+		fmt.Println(offer_list)
+
+		// accpet_offer_list := *crud.AcceptOfferListUser(uint(uid), db)
+		// responseOK(c, accpet_offer_list)
+		responseOK(c, offer_list)
 		// c.JSON(http.StatusOK, accpet_offer_list)
 	}
 
 }
+
+/*
+select * from
+accept_offer
+NATURAL JOIN offer
+where user_id = 1;
+*/
