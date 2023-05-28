cbd0a68b31309459667aa4e22dd6708aa144d262
hch <>
2023-05-28 10:52:02 +0800

    初始化数据库内容的文件

---
+++ test_bench/first_execute.sh
@@ -0,0 +1,8 @@
+echo "----------- init ------------"
+echo "----------- 初始化数据库中的数据 ------------"
+sh register_test.sh
+sh login.sh
+sh offer_post.sh
+sh accept_offer.sh
+sh complete_offer.sh
+sh user_rating.sh
